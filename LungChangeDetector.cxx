98dc6bef52b7c102024aec012e0e0253d60ab7cd
Christopher Erwin <chrisferwin@gmail.com>
2017-03-20 13:29:58 -0700

    Modify to load two separate image sets (with different readers).

--- LungChangeDetector.cxx
+++ LungChangeDetector.cxx
@@ -13,35 +13,55 @@ int main(int argc, char **argv) {
 
     // Define variables
     NameGeneratorType::Pointer nameGenerator = NameGeneratorType::New();
-    ReaderType::Pointer reader = ReaderType::New();
-
+    
     // Accept input or display usage message
-    if (argc == 4) {
-        nameGenerator->SetSeriesFormat(argv[1]);
-        nameGenerator->SetStartIndex(std::stoi(argv[2]));
-        nameGenerator->SetEndIndex(std::stoi(argv[3]));
-    }
-    else {
+    if (argc != 7) {
+        // TODO: UPDATE MESSAGE
         std::cout << "USAGE: " << std::endl;
-        std::cout << "LungChangeDetector.exe <File Path Template> <Start Index> <End Index>" << std::endl;
-        std::cout << "File Path Template -- A standardized file name/path for each numbered image" << std::endl;
+        std::cout << "LungChangeDetector.exe <File Path Template for Set 1> <Start Index> <End Index> <File Path Template for Set 2> <Start Index <End Index>" << std::endl;
+        std::cout << "File Path Template X -- A standardized file name/path for each numbered image" << std::endl;
         std::cout << "      with \"%d\" standing in for the number, such as \"C:\\foo %d.tif\" for" << std::endl;
         std::cout << "      files foo 1.tif, foo 2.tif, etc." << std::endl;
         std::cout << "Start Index -- Number of the first image." << std::endl;
         std::cout << "End Index -- Number of the last image." << std::endl << std::endl;
         std::cout << "For Example:" << std::endl;
-        std::cout << "LungChangeDetector.exe \"C:\\images\\img (%d).tif\" 1 404" << std::endl;
-        std::cout << "To load files img (1).tif through img (404).tif" << std::endl;
+        std::cout << "LungChangeDetector.exe \"C:\\images\\baseline (%d).tif\" 1 404 \"C:\\images\\later (%d).tif\" 1 404" << std::endl;
+        std::cout << "To load files baseline (1).tif through baseline (404).tif" << std::endl;
+        std::cout << "and compare with later (1).tif through later (404).tif" << std::endl;
         return 1;
     }
 
-    // Generate file paths for the reader
+    // Import Baseline Series
+    //
+    ReaderType::Pointer baselineReader = ReaderType::New();
+
+    // Generate file paths
+    nameGenerator->SetSeriesFormat(argv[1]);
+    nameGenerator->SetStartIndex(std::stoi(argv[2]));
+    nameGenerator->SetEndIndex(std::stoi(argv[3]));
     nameGenerator->SetIncrementIndex(1);
     std::vector<std::string> filePaths = nameGenerator->GetFileNames();
 
     // Load slice image files into memory with series reader.
-    reader->SetFileNames(filePaths);
-    reader->Update();
+    baselineReader->SetFileNames(filePaths);
+    baselineReader->Update();
+
+    // Import Later Series
+    //
+    ReaderType::Pointer laterReader = ReaderType::New();
+
+    // Generate file paths
+    nameGenerator->SetSeriesFormat(argv[4]);
+    nameGenerator->SetStartIndex(std::stoi(argv[5]));
+    nameGenerator->SetEndIndex(std::stoi(argv[6]));
+    nameGenerator->SetIncrementIndex(1);
+    filePaths = nameGenerator->GetFileNames();
+
+    // Load slice image files into memory with series reader.
+    laterReader->SetFileNames(filePaths);
+    laterReader->Update();
+
+
 
 	return 0;
 }
